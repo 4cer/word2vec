72a7611ec38456ecc377454b22351eb59c4d3c0a
4cer <4463742+4cer@users.noreply.github.com>
2026-03-21 00:35:26 +0100

    Nonlinearities implementation adin

--- com/nonlinearity.py
+++ com/nonlinearity.py
@@ -3,31 +3,67 @@ import numpy as np
 
 
 class NonLinearity(ABC):
-    @abstractmethod
-    def forward(self, input) -> np.ndarray: ...
+    @property
+    def cache(self) -> np.ndarray | None:
+        return self._cache
+    @cache.setter
+    def cache(self, value: np.ndarray):
+        self._cache = value
 
     @abstractmethod
-    def back(self, input) -> np.ndarray: ...
+    def forward(self, input: np.ndarray) -> np.ndarray: ...
+
+    @abstractmethod
+    def forward_caching(self, input: np.ndarray) -> np.ndarray: ...
+
+    @abstractmethod
+    def back(self, input: np.ndarray) -> np.ndarray: ...
 
 
 class ReLU(NonLinearity):
-    def forward(self, input) -> np.ndarray:
-        raise NotImplementedError()
-
-    def back(self, input) -> np.ndarray:
-        raise NotImplementedError()
+    def forward(self, input: np.ndarray) -> np.ndarray:
+        np.maximum(0, input, out=input)
+        return input
     
-class SoftMax(NonLinearity):
-    def forward(self, input) -> np.ndarray:
-        raise NotImplementedError()
+    def forward_caching(self, input: np.ndarray) -> np.ndarray:
+        np.maximum(0, input, out=input)
+        self.cache = np.copy(input)
+        return input
 
-    def back(self, input) -> np.ndarray:
-        raise NotImplementedError()
+    def back(self, input: np.ndarray) -> np.ndarray:
+        if self.cache is None:
+            raise ValueError("ReLU: Failed to find cached forward pass!")
+        grad_x = input * (self.cache > 0).astype(input.dtype)
+        return grad_x
+
+
+class SoftMax(NonLinearity):
+    def forward(self, input: np.ndarray) -> np.ndarray:
+        exp = np.exp(input)
+        return exp / np.sum(exp)
+    
+    def forward_caching(self, input: np.ndarray) -> np.ndarray:
+        x = 1 / (1 + np.exp(-input))
+        self.cache = np.copy(x)
+        return x
+
+    def back(self, input: np.ndarray) -> np.ndarray:
+        if self.cache is None:
+            raise ValueError("SoftMax: Failed to find cached forward pass!")
+        pass
+
 
 
 class Sigmoid(NonLinearity):
-    def forward(self, input) -> np.ndarray:
-        raise NotImplementedError()
+    def forward(self, input: np.ndarray) -> np.ndarray:
+        return 1 / (1 + np.exp(-input))
+    
+    def forward_caching(self, input: np.ndarray) -> np.ndarray:
+        x = 1 / (1 + np.exp(-input))
+        self.cache = np.copy(x)
+        return x
 
-    def back(self, input) -> np.ndarray:
-        raise NotImplementedError()
+    def back(self, input: np.ndarray) -> np.ndarray:
+        if not self.cache:
+            raise ValueError("Sigmoid: Failed to find cached forward pass!")
+        return input * self.cache * (1 - self.cache)
